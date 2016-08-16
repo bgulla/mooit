b13fbe66234e54f91dd135d857c81cfa3e7708ae
Brandon Gulla <hey@brandongulla.com>
2016-08-16 18:20:19 -0400

    added logging

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash, g, abort
 from functools import wraps
+import logging
 import cowsay
+from logging.handlers import RotatingFileHandler
+
 
 app = Flask(__name__)
 
@@ -10,7 +13,9 @@ def home():
     cow = cowsay.cowsay("moo")
     if request.method == 'POST':
         if request.form['text']:
-            cow = cowsay.cowsay(request.form['text'])
+            moo_text = request.form['text']
+            cow = cowsay.cowsay(moo_text)
+            app.logger.info('[Moo] ' + request.remote_addr + "\t" + moo_text)
     return render_template("index.html", cow=cow)
 
 # API
@@ -20,14 +25,19 @@ def api():
         if request.form['text']:
             return cowsay.cowsay(request.form['text'])
     else:
+
         abort(400, 'missing POST[test] field')
     return
 
 # Custom 404 page
 @app.errorhandler(404)
 def page_not_found(e):
+    app.logger.error('[ERROR] ' + request.remote_addr + "\t404\t" + request.url)
     return render_template('404.html'), 404
 
 # Start the server with the 'run()' method
 if __name__ == '__main__':
+    handler = RotatingFileHandler('/tmp/mooit.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run(host="0.0.0.0",debug=True)
