68d8fb343b8c3b42d6848d1afaceb8721cacbe1a
brando <brandon@brandongulla.com>
2018-11-27 18:57:12 -0500

    app to application

--- wsgi.py
+++ wsgi.py
@@ -2,17 +2,17 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 from OpenSSL import SSL
-from mooit import app
+from mooit import application
 
 if __name__ == "__main__":
 #    handler = RotatingFileHandler('access.log', maxBytes=10000, backupCount=1)
 #    handler.setLevel(logging.INFO)
-#    app.logger.addHandler(handler)
+#    application.logger.addHandler(handler)
     context = SSL.Context(SSL.SSLv23_METHOD)
     cert_file = '/secret/cert/python-tls.crt'
     key_file = '/secret/cert/python-tls.key'
     if not os.path.exists('/secret/cert'):
         cert_file = './python-tls.crt'
         key_file = './python-tls.key'
-    app.run(host="0.0.0.0",port=8443,ssl_context=(cert_file,key_file), debug=True)
+    application.run(host="0.0.0.0",port=9443,ssl_context=(cert_file,key_file))
 
